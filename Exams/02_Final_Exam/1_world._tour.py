5aa91cc76cc50835e1da47fdffac3f4e17e006a9
tankishev <98879302+tankishev@users.noreply.github.com>
2022-02-06 23:24:54 +0200

    Added problem descriptions

--- Exams/02_Final_Exam/1_world._tour.py
+++ Exams/02_Final_Exam/1_world._tour.py
@@ -1,3 +1,22 @@
+# On the first line, you will be given a string containing all of your stops. 
+# Until you receive the command "Travel", you will be given some commands to manipulate that initial string. The commands can be:
+# •	"Add Stop:{index}:{string}":
+# o	Insert the given string at that index only if the index is valid
+# •	"Remove Stop:{start_index}:{end_index}":
+# o	Remove the elements of the string from the starting index to the end index (inclusive) if both indices are valid
+# •	"Switch:{old_string}:{new_string}":
+# o	If the old string is in the initial string, replace it with the new one (all occurrences)
+# Note: After each command, print the current state of the string
+# After the "Travel" command, print the following: "Ready for world tour! Planned stops: {string}"
+
+# Input / Constraints
+# •	JavaScript: you will receive a list of strings
+# •	An index is valid if it is between the first and the last element index (inclusive) in the sequence.
+
+# Output
+# •	Print the proper output messages in the proper cases as described in the problem description
+
+
 stops = input()
 while True:
     input_line = input()
--- Exams/02_Midterm_Exam/3_numbers.py
+++ Exams/02_Midterm_Exam/3_numbers.py
@@ -1,12 +1,15 @@
-txt = input()
-lst = txt.split()
-total = 0
+# Input
+# •	Read from the console a single line holding space-separated integers.
+# Output
+# •	Print the above-described numbers on a single line, space-separated. 
+# •	If less than 5 numbers hold the property mentioned above, print less than 5 numbers. 
+# •	Print "No" if no numbers hold the above property.
+# Constraints
+# •	All input numbers are integers in the range [-1 000 000 … 1 000 000]. 
+# •	The count of numbers is in the range [1…10 000].
 
-for i in range(len(lst)):
-    lst[i]=int(lst[i])
-    total += lst[i]
-
-average = total / len(lst)
+lst = [int(x) for x in input().split(' ')]
+average = sum(lst) / len(lst)
 lst.sort(reverse=True)
 
 result = ''
