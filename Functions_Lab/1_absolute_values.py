dc6c489f492106238fcfbafa279a52ab8e6e0b25
tankishev <98879302+tankishev@users.noreply.github.com>
2022-02-07 14:54:40 +0200

    small update

--- Functions_Lab/1_absolute_values.py
+++ Functions_Lab/1_absolute_values.py
@@ -1,9 +1,9 @@
 # Write a program that receives a sequence of numbers, 
 # separated by a single space, and prints their absolute value as a list. Use abs().
 
-def abs_float(x):
-    return abs(float(x))
+def abs_float(lst: list) -> list:
+    return [abs(float(x)) for x in lst]
 
 input_data = input().split(' ')
-num_list = [abs_float(x) for x in input_data]
+num_list = abs_float(input_data)
 print(num_list)
