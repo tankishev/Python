07077d3284d05671b1e28f5d21ea7b735288c61e
tankishev <98879302+tankishev@users.noreply.github.com>
2022-02-26 08:17:36 +0200

    Beautify code

--- Advanced/Exercises/Multi_Dimentional_Lists_Exercise_2/6_range_day.py
+++ Advanced/Exercises/Multi_Dimentional_Lists_Exercise_2/6_range_day.py
@@ -1,14 +1,21 @@
-# You will be given a matrix with 5 rows and 5 columns. It is a shotgun range represented as some symbols separated by a single space:
+# You will be given a matrix with 5 rows and 5 columns.
+# It is a shotgun range represented as some symbols separated by a single space:
 # •	Your position is marked with the symbol "A"
 # •	Targets marked with symbol "x"
-# •	All of the empty positions will be marked with "."
-# After the field state, you will be given an integer representing the number of commands you will receive. The possible commands are:
-# •	"move {right/left/up/down} {steps}" – you should move in the given direction with the given steps. You can only move if the field you want to step on is marked with ".".
-# •	"shoot {right/left/up/down}" – you should shoot in the given direction (from your current position without moving). Beware that there might be targets that stand in the way of other targets, and you cannot reach them - you can shoot only the nearest target. When you have shot a target, the field becomes empty position (".").
+# •	All empty positions will be marked with "."
+# After the field state, you will be given an integer representing the number of commands you will receive.
+# The possible commands are:
+# •	"move {right/left/up/down} {steps}" – you should move in the given direction with the given steps.
+#       You can only move if the field you want to step on is marked with ".".
+# •	"shoot {right/left/up/down}" – you should shoot in the given direction (from your current position without moving).
+#       Beware that there might be targets that stand in the way of other targets, and you cannot reach them - you can
+#       shoot only the nearest target. When you have shot a target, the field becomes empty position (".").
 # Validate the positions since they can be outside the field.
 # Keep track of all the shot targets:
-# •	If at any point there are no targets left, end the program and print: "Training completed! All {count_targets} targets hit.". 
-# •	If, after you perform all the commands, there are some targets left print: "Training not completed! {count_left_targets} targets left.".
+# •	If at any point there are no targets left, end the program and print:
+#       "Training completed! All {count_targets} targets hit.".
+# •	If, after you perform all the commands, there are some targets left print:
+#       "Training not completed! {count_left_targets} targets left.".
 # Finally, print the index positions of the targets that you hit, as shown in the examples.
 # Input
 # •	5 lines representing the field (symbols, separated by a single space)
@@ -32,7 +39,8 @@ move = {
 
 
 def move_a_cell(matrix: list, cell: tuple, direction: str, steps: int) -> tuple:
-    ''' Move the shooter cell. If new cell is out of range returns the current cell '''
+    """ Move the shooter cell. If new cell is out of range returns the current cell """
+
     new_cell = move[direction](cell, int(steps))
     if cell_valid(new_cell):
         if get_value(matrix, new_cell) in ('.', 'A'):
@@ -41,7 +49,7 @@ def move_a_cell(matrix: list, cell: tuple, direction: str, steps: int) -> tuple:
 
 
 def cell_valid(cell: tuple) -> bool:
-    ''' Validates if cell is out of range'''
+    """ Validates if cell is out of range"""
     if 0 <= min(cell) and max(cell) < 5:
         return True
     else:
@@ -49,8 +57,8 @@ def cell_valid(cell: tuple) -> bool:
 
 
 def shoot(matrix: list, cell: tuple, direction: str) -> tuple:
-    ''' Finds an 'x' cell in the firing direction. \
-        Returns none if no targes in that direction'''
+    """ Finds an 'x' cell in the firing direction. \
+        Returns none if no targets in that direction"""
     target_cell = move[direction](cell, 1)
     while cell_valid(target_cell):
         if get_value(matrix, target_cell) == 'x':
@@ -59,13 +67,13 @@ def shoot(matrix: list, cell: tuple, direction: str) -> tuple:
 
 
 def get_value(matrix: list, cell: tuple) -> str:
-    ''' Gets the value at the x,y coordinates in the matrix'''
+    """ Gets the value at the x,y coordinates in the matrix"""
     row, col = cell
     return str(matrix[row][col])
 
 
 def set_value(matrix: list, cell: tuple, value: str) -> list:
-    ''' Changes the value at the x, y coordinates in the matrix '''
+    """ Changes the value at the x, y coordinates in the matrix """
     row, col = cell
     matrix[row][col] = value
     return matrix
