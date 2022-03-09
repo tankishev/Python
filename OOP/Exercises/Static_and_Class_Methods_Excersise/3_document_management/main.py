f1565e10c4882241180e222d7c47b0bb08fbe5b1
tankishev <98879302+tankishev@users.noreply.github.com>
2022-03-09 19:22:05 +0200

    Initial commit

--- OOP/Exercises/Static_and_Class_Methods_Excersise/3_document_management/main.py
+++ OOP/Exercises/Static_and_Class_Methods_Excersise/3_document_management/main.py
@@ -9,3 +9,13 @@ d1 = Document(1, 1, 1, "finilize project")
 
 d1.add_tag("urgent")
 d1.add_tag("work")
+
+storage = Storage()
+storage.add_category(c1)
+storage.add_topic(t1)
+storage.add_document(d1)
+
+print(c1)
+print(t1)
+print(storage.get_document(1))
+print(storage)
