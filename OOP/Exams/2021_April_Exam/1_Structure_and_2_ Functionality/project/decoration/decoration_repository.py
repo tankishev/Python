334ac8b55dfb24b52ebaf482346fc03f5479e6be
tankishev <98879302+tankishev@users.noreply.github.com>
2022-03-17 21:42:18 +0200

    Initial commit

---
+++ OOP/Exams/2021_April_Exam/1_Structure_and_2_ Functionality/project/decoration/decoration_repository.py
@@ -0,0 +1,23 @@
+from project.decoration.base_decoration import BaseDecoration
+
+
+class DecorationRepository:
+
+    def __init__(self) -> None:
+        self.decorations = []
+
+    def add(self, decoration: BaseDecoration) -> None:
+        if decoration not in self.decorations:
+            self.decorations.append(decoration)
+
+    def remove(self, decoration: BaseDecoration) -> bool:
+        if decoration in self.decorations:
+            self.decorations.remove(decoration)
+            return True
+        return False
+
+    def find_by_type(self, decoration_type: str):
+        for decoration in self.decorations:
+            if decoration.decoration_type == decoration_type:
+                return decoration
+        return 'None'
