da524ae48c9a76023c920560dd47e713a0718495
tankishev <98879302+tankishev@users.noreply.github.com>
2022-03-19 16:51:27 +0200

    Initial commit

--- OOP/Exams/2020_August_Retake/3_Unit_Tests/project/test/test.py
+++ OOP/Exams/2020_August_Retake/3_Unit_Tests/project/test/test.py
@@ -7,91 +7,110 @@ import unittest
 
 class StudentReportCardTests(unittest.TestCase):
 
+    def setUp(self) -> None:
+        self.card = StudentReportCard('Bobi', 10)
+
     def test_init(self):
-        s = StudentReportCard('Bobi', 10)
-        self.assertEqual(s.student_name, 'Bobi')
-        self.assertEqual(s.school_year, 10)
-        self.assertTrue(hasattr(s, 'grades_by_subject'))
-        self.assertTrue(hasattr(s, 'student_name'))
-        self.assertTrue(hasattr(s, 'school_year'))
-        self.assertTrue(hasattr(s, 'add_grade'))
-        self.assertTrue(hasattr(s, 'average_grade_by_subject'))
-        self.assertTrue(hasattr(s, 'average_grade_for_all_subjects'))
-        self.assertTrue(hasattr(s, f'_StudentReportCard__student_name'))
-        self.assertTrue(hasattr(s, f'_StudentReportCard__school_year'))
-        self.assertTrue(isinstance(s.grades_by_subject, dict))
-        self.assertDictEqual(s.grades_by_subject, {})
+        student_name = 'Bobi'
+        school_year = 10
+        grades = {}
+        self.assertEqual(student_name, self.card.student_name)
+        self.assertEqual(school_year, self.card.school_year)
+        self.assertDictEqual(grades, self.card.grades_by_subject)
 
-    def test_prop_setters(self):
-        s = StudentReportCard('Bobi', 10)
-        self.assertEqual(getattr(s, f'_StudentReportCard__student_name', None), 'Bobi')
-        self.assertEqual(getattr(s, f'_StudentReportCard__school_year', None), 10)
-        self.assertEqual(s.student_name, 'Bobi')
-        self.assertEqual(s.school_year, 10)
-        s.student_name = 'Ivo'
-        s.school_year = 7
-        self.assertEqual(getattr(s, f'_StudentReportCard__student_name', None), 'Ivo')
-        self.assertEqual(getattr(s, f'_StudentReportCard__school_year', None), 7)
+    def test_name_setter_valid(self):
+        student_name = 'Dean'
+        self.card.student_name = student_name
+        self.assertEqual(student_name, self.card.student_name)
 
-    def test_student_name_raises_error_if_blank(self):
+    def test_name_setter_error(self):
         error_text = "Student Name cannot be an empty string!"
-        s = StudentReportCard('Bobi', 10)
-        with self.assertRaises(ValueError) as context:
-            s.student_name = ''
-        self.assertTrue(error_text in str(context.exception))
+        student_name = ''
 
         with self.assertRaises(ValueError) as context:
-            s2 = StudentReportCard('', 8)
-        self.assertTrue(error_text in str(context.exception))
+            self.card.student_name = student_name
+        self.assertEqual(error_text, str(context.exception))
 
-    def test_school_year_raises_error_if_not_between_1_and_12(self):
+        with self.assertRaises(ValueError) as context:
+            StudentReportCard(student_name, 12)
+        self.assertEqual(error_text, str(context.exception))
+
+    def test_school_year_setter_valid(self):
+        school_year = 9
+        self.card.school_year = school_year
+        self.assertEqual(school_year, self.card.school_year)
+
+    def test_school_year_less_than_1(self):
         error_text = "School Year must be between 1 and 12!"
-        s = StudentReportCard('Bobi', 10)
-        with self.assertRaises(ValueError) as context:
-            s.school_year = 0
-        self.assertTrue(error_text in str(context.exception))
+        school_year = 0
 
         with self.assertRaises(ValueError) as context:
-            s.school_year = 13
-        self.assertTrue(error_text in str(context.exception))
+            self.card.school_year = 0
+        self.assertEqual(error_text, str(context.exception))
 
         with self.assertRaises(ValueError) as context:
-            s2 = StudentReportCard("Ivo", 22)
-        self.assertTrue(error_text in str(context.exception))
+            StudentReportCard('Test', school_year)
+        self.assertEqual(error_text, str(context.exception))
 
-    def test_add_grade(self):
-        s = StudentReportCard('Bobi', 10)
-        s.add_grade('Math', 6)
-        self.assertDictEqual(s.grades_by_subject, {'Math': [6]})
-        s.add_grade('Math', 5)
-        self.assertDictEqual(s.grades_by_subject, {'Math': [6, 5]})
-        s.add_grade('English', 5.6)
-        self.assertDictEqual(s.grades_by_subject, {'Math': [6, 5], 'English': [5.6]})
+    def test_school_year_more_than_12(self):
+        error_text = "School Year must be between 1 and 12!"
+        school_year = self.card.school_year
 
-    def test_average_grades(self):
-        s = StudentReportCard('Bobi', 10)
-        self.assertEqual(s.average_grade_by_subject(), "")
-        s.add_grade('Math', 6)
-        s.add_grade('Math', 4)
-        self.assertEqual(s.average_grade_by_subject(), "Math: 5.00")
-        s.add_grade('English', 5.6)
-        self.assertEqual(s.average_grade_by_subject(), "Math: 5.00\nEnglish: 5.60")
-        self.assertEqual(s.average_grade_for_all_subjects(), 'Average Grade: 5.20')
+        with self.assertRaises(ValueError) as context:
+            self.card.school_year = 130
+        self.assertEqual(error_text, str(context.exception))
+        self.assertEqual(school_year, self.card.school_year)
+
+    def test_add_grade_method(self):
+        self.card.grades_by_subject = {}
+
+        retval = self.card.add_grade('Math', 6)
+        self.assertIsNone(retval)
+
+        self.card.add_grade('Math', 5)
+        self.card.add_grade('English', 5.6)
+
+        expected = {'Math': [6, 5], 'English': [5.6]}
+        self.assertDictEqual(expected, self.card.grades_by_subject)
+
+    def test_average_grades_method(self):
+        self.card.grades_by_subject = {}
+
+        expected = ''
+        self.assertEqual(expected, self.card.average_grade_by_subject())
+
+        self.card.add_grade('Math', 6)
+        self.card.add_grade('Math', 4)
+        self.card.add_grade('English', 5.6)
+        self.card.add_grade('English', 3.6)
+
+        expected = "Math: 5.00\nEnglish: 4.60"
+        self.assertEqual(expected, self.card.average_grade_by_subject())
+
+    def test_average_grades_all_subjects_method(self):
+        self.card.grades_by_subject = {}
+
+        self.card.add_grade('Math', 6)
+        self.card.add_grade('Math', 4)
+        self.card.add_grade('English', 5.6)
+
+        expected = 'Average Grade: 5.20'
+        self.assertEqual(expected, self.card.average_grade_for_all_subjects())
 
     def test_repr(self):
-        student_name = 'Bobi'
-        school_year = 9
-        s = StudentReportCard(student_name, school_year)
-        s.add_grade('Math', 6)
-        s.add_grade('Math', 4)
-        s.add_grade('English', 5.6)
-        report = f"Name: {s.student_name}\n" \
-                 f"Year: {s.school_year}\n" \
+        self.card.grades_by_subject = {}
+        self.card.add_grade('Math', 6)
+        self.card.add_grade('Math', 4)
+        self.card.add_grade('English', 5.6)
+
+        report = f"Name: Bobi\n" \
+                 f"Year: 10\n" \
                  f"----------\n" \
-                 f"{s.average_grade_by_subject()}\n" \
+                 f"Math: 5.00\n" \
+                 f"English: 5.60\n" \
                  f"----------\n" \
-                 f"{s.average_grade_for_all_subjects()}"
-        self.assertEqual(repr(s), report)
+                 f"Average Grade: 5.20"
+        self.assertEqual(report, repr(self.card))
 
 
 if __name__ == '__main__':
